74b5e21c2f5f981290ce5d73be47de55b366732f
s4mzai <hi.samar.7x@gmail.com>
2025-12-15 02:42:07 +0530

    Add morphing text animation on button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-
+"use client"
+import { useState } from "react";
+import { TextMorph } from "@/components/ui/text-morph";
 
 export default function Home() {
+  const [text, setText] = useState<string>("Create Room")
+  const [disable, setDisable] = useState<boolean>(false)
   return (
-    <div className="flex flex-col items-center justify-center h-dvh px-4">
+    <div className="flex flex-col items-center justify-center h-dvh px-4 bg-black">
       <div>
         <h1 className="text-green-500 text-4xl font-bold tracking-wide mb-2 text-center">{">"}private_chat</h1>
         <p className="text-zinc-400 text-center text-lg font-medium mb-6 tracking-wide">A private, self-destructing chat room</p>
@@ -10,7 +14,21 @@ export default function Home() {
       <div className="w-full max-w-md mx-auto flex flex-col bg-zinc-900/50 justify-center gap-4 p-6 sm:p-8 shadow-lg border border-zinc-800 min-h-[240px]">
         <h2 className="text-zinc-400">Your Identity</h2>
         <input type="text" value={"hello"} disabled={true} className="bg-black/80 text-zinc-400 p-2 w-full" />
-        <button className="bg-white text-black p-2 w-full hover:bg-white/80 transition-colors">Create Room</button>
+        {/* TODO: ADD A WORK HERE */}
+        <button onClick={()=>{
+            setDisable(true)
+            setText(text == "Create Room"? "Creating Room...":"Create Room")
+            setTimeout(() => {
+              setDisable(false)
+              setText("Create Room")
+            }, 2000)
+          }}
+          className="bg-white text-black p-2 w-full hover:bg-white/80 transition-colors"
+          disabled={disable}
+        >
+
+          <TextMorph>{text}</TextMorph>
+        </button>
       </div>
     </div>
   );
