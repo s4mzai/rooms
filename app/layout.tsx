db868976856e6d0ddf8d87fb842dcf1cefbd89a4
s4mzai <hi.samar.7x@gmail.com>
2025-12-21 18:23:03 +0530

    set up elysia tanstack-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/providers";
 
 const jetbrainsMono = JetBrains_Mono({
 	variable: "--font-jetbrains-mono",
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetbrainsMono.variable} antialiased`}>
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,18 @@ export default function Home() {
     </TypingAnimation>
   ), []);
 
+  const handleClick = () => {
+    setDisable(true)
+    setText(text == "Create Room"? "Creating Room...":"Create Room")
+    console.log(username)
+
+    
+    setTimeout(() => {
+      setDisable(false)
+      setText("Create Room")
+    }, 2000)
+
+  }
   const username = useUsername()
   return (
     <div className="flex flex-col items-center justify-center h-dvh px-4 bg-black">
@@ -26,19 +38,10 @@ export default function Home() {
       <div className="w-full max-w-md mx-auto flex flex-col bg-zinc-900/50 justify-center gap-4 p-6 sm:p-8 shadow-lg border border-zinc-800 min-h-[240px]">
         <h2 className="text-zinc-400">Your Identity</h2>
         <input type="text" value={username} disabled={true} className="bg-black/80 text-zinc-400 p-2 w-full" />
-        {/* TODO: ADD A WORK HERE */}
-        <button onClick={()=>{
-            setDisable(true)
-            setText(text == "Create Room"? "Creating Room...":"Create Room")
-            setTimeout(() => {
-              setDisable(false)
-              setText("Create Room")
-            }, 2000)
-          }}
+        <button onClick={handleClick}
           className="bg-white text-black p-2 w-full hover:bg-white/80 transition-colors"
           disabled={disable}
         >
-
           <TextMorph>{text}</TextMorph>
         </button>
       </div>
