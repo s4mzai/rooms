c79dee0ec20e31753300224c2ce4cf36a99b5292
s4mzai <hi.samar.7x@gmail.com>
2025-12-21 09:46:01 +0530

    fix: typing animation rendering on using username hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 "use client"
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { TextMorph } from "@/components/ui/text-morph";
 import { TypingAnimation } from "@/components/ui/typing-animation"
 import useUsername from "@/hooks/username";
 
 
+
 export default function Home() {
   const [text, setText] = useState<string>("Create Room")
   const [disable, setDisable] = useState<boolean>(false)
+  
+  const MemoTyping = useMemo(() => (
+    <TypingAnimation className="text-zinc-400 text-lg font-medium tracking-wide">
+      A private, self-destructing chat room
+    </TypingAnimation>
+  ), []);
 
   const username = useUsername()
   return (
     <div className="flex flex-col items-center justify-center h-dvh px-4 bg-black">
       <div className="w-full max-w-md mx-auto mb-6 text-center">
         <h1 className="text-green-500 text-4xl font-bold tracking-wide mb-2 ">{">"}private_chat</h1>
-        <TypingAnimation className="text-zinc-400 text-lg font-medium tracking-wide">A private, self-destructing chat room</TypingAnimation>
+        {MemoTyping}
       </div>
       <div className="w-full max-w-md mx-auto flex flex-col bg-zinc-900/50 justify-center gap-4 p-6 sm:p-8 shadow-lg border border-zinc-800 min-h-[240px]">
         <h2 className="text-zinc-400">Your Identity</h2>
