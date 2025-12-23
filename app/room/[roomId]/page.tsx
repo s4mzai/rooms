2fb5acc423718afcc7ff99d37a42629ddbeae394
s4mzai <hi.samar.7x@gmail.com>
2025-12-23 14:45:21 +0530

    Add and fix Ui bottom component: input box & button

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -7,8 +7,8 @@ const Page = ()=>{
     const params = useParams()
     const roomId = params.roomId
     return(
-        <div className="bg-black text-white min-h-dvh">
-            <div className="flex flex-col sm:flex-row gap-5 sm:gap-0 items-center justify-evenly p-5 border-b border-zinc-800/70 bg-zinc-900/40 ">
+        <div className="bg-black text-white min-h-dvh flex flex-col justify-between">
+            <div className="flex flex-col sm:flex-row gap-5 sm:gap-0 items-center justify-evenly p-5 border-b border-zinc-800/70 bg-zinc-900/40">
                 <div className="w-full">
                     <h2 className="uppercase text-zinc-400 text-sm ">ROOM ID</h2>
                     <div className="flex gap-4">
@@ -26,11 +26,19 @@ const Page = ()=>{
                     <button className="relative p-2 text-xs cursor-pointer overflow-hidden rounded-sm bg-zinc-800 hover:bg-zinc-800/80 active:bg-zinc-800 focus:bg-zinc-800 disabled:bg-zinc-800 disabled:opacity-50 text-red-500 font-bold hover:text-red-500/80">DESTROY NOW</button>
                 </div>
             </div>
-            <div>
 
+            <div>
+                {/* MESSAGES LIST COMPONENT */}
             </div>
-            <div>
 
+            <div className="flex gap-5 items-center p-5 border-b border-zinc-800/70 bg-zinc-900/40 h-25 md:px-">
+                <input
+                    type="text"
+                    className="w-full py-2 px-5 border border-zinc-800/90  text-green-500/80 placeholder:text-zinc-500/50 focus:outline-none focus:ring-0"
+                    placeholder="Type Message..."
+                />
+                
+                <button className="uppercase bg-zinc-700 py-2 px-5 cursor-pointer hover:text-white/90">SEND</button>
             </div>
         </div>
     )
